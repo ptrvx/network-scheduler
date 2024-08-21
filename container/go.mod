d3e4dcbe954b4e8f56b44236f366e0c429dd95c4
Petar Vukovic <petar.vukovic@hotmail.com>
2024-08-21 23:21:38 +0200

    k8s client update CRDs

--- container/go.mod
+++ container/go.mod
@@ -2,20 +2,55 @@ module github.com/ptrvx/network-scheduler/container
 
 go 1.23.0
 
-require github.com/BGrewell/go-iperf v0.0.0-20240522025150-6a2b45559210
+require (
+	github.com/BGrewell/go-iperf v0.0.0-20240522025150-6a2b45559210
+	k8s.io/client-go v0.31.0
+)
 
 require (
 	github.com/BGrewell/go-conversions v0.0.0-20201203155646-5e189e4ca087 // indirect
 	github.com/BGrewell/tail v1.0.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hpcloud/tail v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20190311183353-d8887717615a // indirect
-	golang.org/x/sys v0.0.0-20191005200804-aed5e4c7ecf9 // indirect
-	golang.org/x/text v0.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 	google.golang.org/grpc v1.36.1 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.31.0 // indirect
+	k8s.io/apimachinery v0.31.0 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
+	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- container/main.go
+++ container/main.go
@@ -1,17 +1,93 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/BGrewell/go-iperf"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+func main() {
+	_, dynamicClient, err := getKubernetesClient()
+	if err != nil {
+		log.Fatalf("failed to create k8s client: %v", err)
+	}
+
+	gvr := schema.GroupVersionResource{
+		Group:    "raf.rs",
+		Version:  "v1",
+		Resource: "nodemetrics",
+	}
+
+	list, err := dynamicClient.Resource(gvr).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("failed to list NodeMetric CRDs: %v", err)
+	}
+
+	// Print all NodeMetrics
+	fmt.Printf("Listing all NodeMetrics:\n")
+	for _, nm := range list.Items {
+		fmt.Printf("- %s\n", nm.GetName())
+	}
+
+	// Updating a NodeMetric
+	nodeMetricName := "example-nodemetric"
+	nodeMetric, err := dynamicClient.Resource(gvr).Namespace("").Get(context.TODO(), nodeMetricName, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		log.Fatalf("NodeMetric %v does not exist", nodeMetricName)
+	} else if err != nil {
+		log.Fatalf("failed to get NodeMetric: %v", err)
+	}
+
+	metrics, found, err := unstructured.NestedSlice(nodeMetric.Object, "spec", "metrics")
+	if err != nil {
+		log.Fatalf("failed to retrieve metrics from NodeMetric: %v", err)
+	}
+	if !found || metrics == nil {
+		log.Fatalf("failed to find metrics in NodeMetric or is nil")
+	}
+
+	if len(metrics) == 0 {
+		log.Fatalf("failed to update bandwidth, metrics list is empty")
+	}
+
+	metricMap, ok := metrics[0].(map[string]any)
+	if !ok {
+		log.Fatalf("failed to update metric, first metric is not a map")
+	}
+	metricMap["bandwidth"] = "200Mbps"
+	metrics[0] = metricMap
+
+	err = unstructured.SetNestedSlice(nodeMetric.Object, metrics, "spec", "metrics")
+	if err != nil {
+		log.Fatalf("failed to set updated metrics in NodeMetric: %v", err)
+	}
+
+	// Push the updated NodeMetric back to the cluster
+	updated, err := dynamicClient.Resource(gvr).Namespace("").Update(context.TODO(), nodeMetric, metav1.UpdateOptions{})
+	if err != nil {
+		log.Fatalf("failed to update NodeMetric: %v", err)
+	}
+	fmt.Printf("Updated NodeMetric %v successfully!\n", nodeMetricName)
+	fmt.Printf("Updated NodeMetric: %v\n", updated.Object)
+
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
 
-func main() {
+func server() {
 	s := iperf.NewServer()
 	s.SetPort(5201)
 	err := s.Start()
@@ -26,4 +102,20 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
+}
+
+func getKubernetesClient() (*kubernetes.Clientset, dynamic.Interface, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	return clientset, dynamicClient, nil
 }
